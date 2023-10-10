8d00d0fb56624953af9691695b75afe2fdc93466
Qnariktad <147527926+Qnariktad@users.noreply.github.com>
2023-10-10 21:13:16 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+      <div className='parent'>
+        <div className='child'>
+          <h1>book 1</h1>
+          <p>About JS</p>
+        </div>
+        <div className='child'>
+          <h1>book 2</h1>
+          <p>About react</p>
+        </div>
+        <div className='child'>
+          <h1>book 3</h1>
+          <p>About CSS</p>
+        </div>
+      </div>
+  
+  );
+}
+
+export default App;
